08921d4c3a0b878802766eb5536fad2f2f440a0b
roisd <Openthed00r>
2022-08-11 19:57:32 +0700

    add book handler to save book

---
+++ src/handler.js
@@ -0,0 +1,9 @@
+const addBookHandler = (request, h) => {
+  const {
+    name, year, author, summary, publisher, pageCount, readPage, reading,
+  } = request.payload;
+};
+
+module.exports = {
+  addBookHandler,
+};
